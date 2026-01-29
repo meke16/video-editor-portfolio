e7e26173d39c22f350f4b59b689afa688337a4f5
meke16 <habtamucherinet40@gmail.com>
2026-01-29 16:28:02 +0300

    refactor: remove deprecated API files and update routing structure

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 const authRoutes = require('./routes/auth.routes');
 const apiRoutes = require('./routes/api.routes');
 
-app.use('/api/auth', authRoutes);
+app.use('/api', authRoutes);
 app.use('/api', apiRoutes);
 
 app.get('/', (req, res) => {
