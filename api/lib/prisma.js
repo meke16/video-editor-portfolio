216e31470d8be0c20cc249ce1968ba77d6bad731
meke16 <habtamucherinet40@gmail.com>
2026-02-01 21:45:47 +0300

    refactor: remove Prisma integration and obsolete server files for serverless deployment

--- api/lib/prisma.js
+++ api/lib/prisma.js
@@ -1,5 +1,1 @@
-const { PrismaClient } = require('@prisma/client');
-
-const prisma = new PrismaClient();
-
-module.exports = prisma;
+// Prisma removed. This file is no longer needed.
--- api/server.js
+++ api/server.js
@@ -1,26 +1,2 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-
 dotenv.config();
-
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-app.use(cors());
-app.use(express.json());
-
-// Routes
-const authRoutes = require('./routes/auth.routes');
-const apiRoutes = require('./routes/api.routes');
-
-app.use('/api', authRoutes);
-app.use('/api', apiRoutes);
-
-app.get('/', (req, res) => {
-    res.send('Video Editor Portfolio API is running');
-});
-
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+// This file is no longer needed for Vercel serverless deployment. All routes are now serverless functions.
